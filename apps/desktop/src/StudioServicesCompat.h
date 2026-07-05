bf83c042d7b7833b5625f88a8791c5c1bd8931ff
krishavi85 <83726923+krishavi85@users.noreply.github.com>
2026-07-04 22:34:00 -0300

    Add JUCE application properties compatibility adapter

---
+++ apps/desktop/src/StudioServicesCompat.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace juce {
+
+class OmniStemApplicationProperties final : public ApplicationProperties {
+public:
+    PropertiesFile& getUserSettings() {
+        auto* file = ApplicationProperties::getUserSettings();
+        jassert(file != nullptr);
+        return *file;
+    }
+};
+
+} // namespace juce
+
+#define ApplicationProperties OmniStemApplicationProperties
+#include "StudioServices.h"
+#undef ApplicationProperties
